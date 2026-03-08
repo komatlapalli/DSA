640ec67865cb7d08c2bca5a6057dd766c25c1879
komatlapalli <saiteja200062@gmail.com>
2026-03-08 20:02:49 -0300

    added methods for circularQueue with fixed capacity in lists

--- CircularQueue.py
+++ CircularQueue.py
@@ -7,6 +7,71 @@ class Queue:
 
     def __str__(self):
         values = [str(x) for x in self.items]
-        return ''.join(values)
+        return ' '.join(values)
+    
+    def isFull(self):
+        if self.top + 1 == self.start:
+            return True
+        elif self.start == 0 and self.top + 1 == self.maxsize:
+            return True
+        else:
+            return False
+        
+    def isEmpty(self):
+        if self.top == -1:
+            return True
+        else:
+            return False
+        
+    def enqueue(self, value):
+        if self.isFull():
+            return "The queue is full"
+        else:
+            if self.top + 1 == self.maxsize:
+                self.top = 0
+            else:
+                self.top += 1
+                if self.start == -1:
+                    self.start = 0
+            self.items[self.top] = value
+            return "The element is inserted at the end of the queue"
+        
+    def dequeue(self):
+        if self.isEmpty():
+            return "The queue is empty"
+        else:
+            firstElement = self.items[self.start]
+            self.items[self.start] = None
+            if self.start == self.top:
+                self.start = -1
+                self.top = -1
+            elif self.start + 1 == self.maxsize:
+                self.start = 0
+            else:
+                self.start += 1
+            return firstElement
+        
+    def peek(self):
+        if self.isEmpty():
+            return "The queue is empty"
+        else:
+            return self.items[self.start]
+        
+    def delete(self):
+        self.items = self.maxsize * [None]
+        self.top = -1
+        self.start = -1
+        return "The queue is deleted"
+    
+        
+    
+customQueue = Queue(3)
+print(customQueue.enqueue(1))
+print(customQueue.enqueue(2))
+print(customQueue.enqueue(3))
+print(customQueue.delete())
+print(customQueue)
+
+
     
     
