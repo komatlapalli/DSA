99b1eb515703e3f20d2f5b6fbc94568a32b4e091
Komatlapalli, Sai Teja <skomatlapalli@baseng.com>
2026-03-18 10:26:36 -0300

    added tree

--- QueueLinkedList.py
+++ QueueLinkedList.py
@@ -1,42 +1,4 @@
-class Node:
-    def __init__(self, value = None):
-        self.value = value
-        self.next = None
-
-    def __str__(self):
-        return str(self.value)
-    
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-    
-    def __iter__(self):
-        currNode = self.head
-        while currNode:
-            yield currNode
-            currNode = currNode.next
-
-class Queue:
-    def __init__(self):
-        self.linkedlist = LinkedList()
-
-    def __str__(self):
-        values = [str(x) for x in self.linkedlist]
-        return " ".join(values)
-    
-    def enqueue(self, value):
-        newNode = Node(value)
-        if self.linkedlist.head == None:
-            self.linkedlist.head = newNode
-            self.linkedlist.tail = newNode
-        else:
-            self.linkedlist.tail.next = newNode
-            self.linkedlist.tail = newNode
+from multiprocessing
 
 
-customqueue = Queue()
-customqueue.enqueue(1)
-customqueue.enqueue(2)
-customqueue.enqueue(3)
-print(customqueue)
+
