935fa5206a29e656f771ab64c6b89aef163929f4
Sai Teja Komatlapalli <saiteja200062@gmail.com>
2026-04-06 09:33:08 -0300

    added binary tree with python list

---
+++ BinaryTreePL.py
@@ -0,0 +1,9 @@
+class BinaryTree:
+    def __init__(self, size):
+        self.customList = size * [None]
+        self.lastUsedIndex = 0
+        self. maxSize = size
+
+newBT = BinaryTree(8)
+
+print(newBT)
