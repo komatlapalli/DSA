a5bfff4787c4b6e374ae1aa41ad46fccb3530c33
komatlapalli <saiteja200062@gmail.com>
2026-04-15 18:46:20 -0300

    Added Insert method for trie

--- Trie.py
+++ Trie.py
@@ -7,7 +7,20 @@ class Trie:
     def __init__(self):
         self.root = TrieNode()
 
+    def insert(self, word):
+        currentNode = self.root
+        for char in word:
+            if char not in currentNode.children:
+                currentNode.children[char] = TrieNode()
+            currentNode = currentNode.children[char]
+        currentNode.endOfString = True
+
 
 newTrie = Trie()
-print(newTrie.root.children)  # Output: {}
-print(newTrie.root.endOfString)  # Output: False
+newTrie.insert("apple")
+print(newTrie.root.children)
+print(newTrie.root.children['a'].children)
+print(newTrie.root.children['a'].children['p'].children)  # Output: {'a': <__main__.TrieNode object at 0x...>   }
+print(newTrie.root.children['a'].children['p'].children['p'].children)  # Output: {'l': <__main__.TrieNode object at 0x...>   }
+print(newTrie.root.children['a'].children['p'].children['p'].children['l'].children)  # Output: {'e': <__main__.TrieNode object at 0x...>   }
+print(newTrie.root.children['a'].children['p'].children['p'].children['l'].children['e'].endOfString)  # Output: True                           
