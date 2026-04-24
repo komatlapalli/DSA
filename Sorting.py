a1c57b89e85d06fc30c095c1f213e778181a9245
komatlapalli <saiteja200062@gmail.com>
2026-04-24 18:51:40 -0300

    added merge sort

--- Sorting.py
+++ Sorting.py
@@ -28,9 +28,55 @@ def insertionSort(customList):
             j-=1
     print(customList)
 
+def merge(customList, l, m, r):
+    n1 = m - l + 1
+    n2 = r - m
+
+    L = [0] * (n1)
+    R = [0] * (n2)
+
+    for i in range(0, n1):
+        L[i] = customList[l + i]
+
+    for j in range(0, n2):
+        R[j] = customList[m + 1 + j]
+
+    i = j = 0
+    k = l
+
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            customList[k] = L[i]
+            i += 1
+        else:
+            customList[k] = R[j]
+            j += 1
+        k += 1
+
+    while i < n1:
+        customList[k] = L[i]
+        i += 1
+        k += 1
+
+    while j < n2:
+        customList[k] = R[j]
+        j += 1
+        k += 1
+
+def mergeSort(customList, l, r):
+    if l < r:
+        m = (l + r) // 2
+        mergeSort(customList, l, m)
+        mergeSort(customList, m + 1, r)
+        merge(customList, l, m, r)
+
+    return customList
+
+
             
 
 
 customList = [1,9,8,5,2,4,3]
 print(customList)
-insertionSort(customList)
+print(mergeSort(customList, 0, len(customList)-1))
+#nsertionSort(customList)
